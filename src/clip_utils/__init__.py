5e9bbfafaceedddc2d798f01ba93c510f4c64462
vrey <valerian.rey@epfl.ch>
2022-08-27 18:06:21 +0200

    Add encode_image function to clip_utils

--- src/clip_utils/__init__.py
+++ src/clip_utils/__init__.py
@@ -2,4 +2,4 @@
 This module contains utility functions involving CLIP.
 """
 
-from clip_utils.clip_encoder import encode_text, load_clip_model
+from clip_utils.clip_encoder import encode_image, encode_text, load_clip_model
